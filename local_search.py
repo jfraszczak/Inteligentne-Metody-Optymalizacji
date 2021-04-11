57b5f56c2b5bacacf714c64c539381d29819480b
jakub <jakub.fraszczak37@gmail.com>
2021-04-11 17:08:21 +0200

    Measurements

--- local_search.py
+++ local_search.py
@@ -5,6 +5,8 @@ import random
 from copy import deepcopy
 from time import perf_counter
 import numpy as np
+import csv
+
 
 # Klasa instancji, przechowuje macierz odległości, listę wierzchołków, liczbę wierzchołków (teraz całość jest osobno zamaist tak jak poprrzednio jako część klasy algorytm żeby troche bardziej zmodularyzować)
 class Instance:
@@ -40,7 +42,8 @@ class Instance:
 
         for i in range(n):
             for j in range(i + 1, n):
-                distance = round(math.sqrt((self.vertices[i].x - self.vertices[j].x) ** 2 + (self.vertices[i].y - self.vertices[j].y) ** 2))
+                distance = round(math.sqrt(
+                    (self.vertices[i].x - self.vertices[j].x) ** 2 + (self.vertices[i].y - self.vertices[j].y) ** 2))
                 self.distance_matrix[i][j] = distance
                 self.distance_matrix[j][i] = distance
 
@@ -68,8 +71,8 @@ class Solution:
             vertices.remove(vertex)
         self.path2 = vertices[:]
 
-    def greedy_algorithm_initialization(self, algorithm):
-        alg = algorithm()
+    def greedy_algorithm_initialization(self):
+        alg = GreedyCycle()
         alg.read_instance_object(self.instance)
         alg.find_solution()
         self.path1 = alg.path1[:]
@@ -141,7 +144,7 @@ class Solution:
     def path2_size(self):
         return len(self.path2)
 
-    def visualise(self, title=None):
+    def visualise(self, title=None, save=False):
         x = []
         y = []
 
@@ -176,7 +179,12 @@ class Solution:
 
         if title is not None:
             plt.title(title)
-        plt.show()
+
+        if not save:
+            plt.show()
+        else:
+            plt.savefig('visualisations/' + title + '.png')
+            plt.clf()
 
 
 # Pierwszy z 2 algorytmów, jeszcze nie wiem które ruchy mamy wykonywać w jakiej kombinacji więc wrzuciłem wszystkie 5.
@@ -230,19 +238,20 @@ def greedy_search(solution: Solution, moves):
         random.shuffle(moves)
         found_better = False
 
+        random.shuffle(moves)
         for move in moves:
             arg1_list = list(range(solution.path1_size()))
             random.shuffle(arg1_list)
             for arg1 in arg1_list:
-                    arg2_list = deepcopy(arg1_list)
-                    arg2_list.remove(arg1)
-                    random.shuffle(arg2_list)
-                    for arg2 in arg2_list:
-                        length = solution.cumulative_length_after_move(getattr(solution, move), arg1, arg2)
-                        if length < best_length:
-                            found_better = True
-                            best_length = length
-                            getattr(solution, move)(arg1, arg2)
+                arg2_list = deepcopy(arg1_list)
+                arg2_list.remove(arg1)
+                random.shuffle(arg2_list)
+                for arg2 in arg2_list:
+                    length = solution.cumulative_length_after_move(getattr(solution, move), arg1, arg2)
+                    if length < best_length:
+                        found_better = True
+                        best_length = length
+                        getattr(solution, move)(arg1, arg2)
 
     return solution
 
@@ -254,60 +263,122 @@ def random_walk(solution: Solution, max_time: float):
              'swap_edges_path2',
              'exchange_vertices']
 
+    best_length = solution.cumulative_length()
+    best_solution = deepcopy(solution)
+
     time_start = perf_counter()
 
-    while (perf_counter()-time_start < max_time):
+    while (perf_counter() - time_start < max_time):
         random.shuffle(moves)
         for move in moves:
             arg1_list = list(range(solution.path1_size()))
             random.shuffle(arg1_list)
             for arg1 in arg1_list:
-                    arg2_list = deepcopy(arg1_list)
-                    arg2_list.remove(arg1)
-                    random.shuffle(arg2_list)
-                    for arg2 in arg2_list:
-                        getattr(solution, move)(arg1, arg2)
+                arg2_list = deepcopy(arg1_list)
+                arg2_list.remove(arg1)
+                random.shuffle(arg2_list)
+                for arg2 in arg2_list:
+                    length = solution.cumulative_length_after_move(getattr(solution, move), arg1, arg2)
+                    if length < best_length:
+                        best_length = length
+                        best_solution = deepcopy(solution)
+                    getattr(solution, move)(arg1, arg2)
 
-    return solution
+    return best_solution
 
 
-instances = [Solution('kroA100.tsp'), Solution('kroB100.tsp')]
+def measurements(num_of_measurements=10):
+    instances = ['kroA100.tsp', 'kroB100.tsp']
+    initializations = ['random_initialization', 'greedy_algorithm_initialization']
+    algorithms = [steepest_search, greedy_search]
 
-max_time = 0
+    movesets = [
+        ['swap_vertices_path1',
+         'swap_vertices_path2',
+         'swap_edges_path1',
+         'swap_edges_path2'],
 
-results = []
+        ['swap_vertices_path1',
+         'swap_vertices_path2',
+         'exchange_vertices']
+    ]
 
-for _ in range(0, 10):
-    line = []
-    for s in instances:
-        s.greedy_algorithm_initialization(GreedyCycle)
-        line.append(s.cumulative_length())
+    max_time = 0
+    results = []
+    times = []
 
-        algorithms = [steepest_search, greedy_search]
-        for fun in algorithms:
-            movesets = [
-                ['swap_vertices_path1',
-                'swap_vertices_path2',
-                'swap_edges_path1',
-                'swap_edges_path2'],
-                ['exchange_vertices']
-            ]
-            for moves in movesets:
-                time_start = perf_counter()
-                sol = fun(deepcopy(s), moves)
-                time = perf_counter() - time_start
-                if time > max_time:
-                    max_time = time
+    best_solutions = dict()
 
-                line.append(sol.cumulative_length())
+    for _ in range(num_of_measurements):
+        line = []
+        line_times = []
+        for instance in instances:
+            s = Solution(instance)
+            for initialization in initializations:
+                initial_solution = deepcopy(s)
+                getattr(initial_solution, initialization)()
 
-        sol = random_walk(deepcopy(s), max_time)
-        line.append(sol.cumulative_length())
+                line.append(initial_solution.cumulative_length())
 
-    results.append(line)
+                for fun in algorithms:
+                    for i_move, moves in enumerate(movesets):
+                        time_start = perf_counter()
 
-results = np.array(results)
-print(np.amin(results, 0))
-print(np.amax(results, 0))
-print(np.mean(results, 0))
-print(np.std(results, 0))
+                        solution = fun(deepcopy(initial_solution), moves)
+
+                        time = perf_counter() - time_start
+                        if time > max_time:
+                            max_time = time
+
+                        line_times.append(time)
+                        line.append(solution.cumulative_length())
+
+                        key = instance + ' ' + initialization + ' ' + fun.__name__ + ' ' + str(i_move)
+                        if key not in best_solutions:
+                            best_solutions[key] = deepcopy(solution)
+                        else:
+                            if best_solutions[key].cumulative_length() > solution.cumulative_length():
+                                best_solutions[key] = deepcopy(solution)
+
+                solution = random_walk(deepcopy(initial_solution), max_time)
+                line.append(solution.cumulative_length())
+
+        results.append(line)
+        times.append(line_times)
+
+    results = np.array(results)
+    minimum = np.amin(results, 0)
+    maximum = np.amax(results, 0)
+    std = np.std(results, 0)
+    mean = np.mean(results, 0)
+    results = [minimum, maximum, std, mean]
+    results = np.transpose(np.array(results))
+
+    times = np.array(times)
+    minimum_time = np.amin(times, 0)
+    maximum_time = np.amax(times, 0)
+    std_time = np.std(times, 0)
+    mean_time = np.mean(times, 0)
+    times = [minimum_time, maximum_time, std_time, mean_time]
+    times = np.transpose(np.array(times))
+
+    with open('results.csv', "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(['min', 'max', 'std', 'mean'])
+        for line in results:
+            line = [str(a) for a in list(line)]
+            writer.writerow(line)
+
+    with open('times.csv', "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(['min', 'max', 'std', 'mean'])
+        for line in times:
+            line = [str(a) for a in list(line)]
+            writer.writerow(line)
+
+    for key in best_solutions:
+        solution = best_solutions[key]
+        print(solution.cumulative_length())
+        solution.visualise(title=key, save=True)
+
+measurements(10)
